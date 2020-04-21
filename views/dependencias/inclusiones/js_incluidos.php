44ff26ca72ec27b66b342610bf808629a0cba8f1
David Dark <32287340+DavidDark@users.noreply.github.com>
2020-04-20 21:46:12 -0500

    Add files via upload

---
+++ views/dependencias/inclusiones/js_incluidos.php
@@ -0,0 +1,18 @@
+   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="dependencias/js/jquery-3.4.1.js"></script>
+    <script src="dependencias/js/popper.min.js"></script>
+    <script src="dependencias/js/bootstrap.js"></script>
+    <script src="dependencias/js/mis_funciones_js.js"></script>
+    <script src="dependencias/js/bootbox/bootbox.js"></script>
+     
+
+<script type="text/javascript" src="dependencias/JSZip-2.5.0/jszip.min.js"></script>
+<script type="text/javascript" src="dependencias/pdfmake-0.1.36/pdfmake.min.js"></script>
+<script type="text/javascript" src="dependencias/pdfmake-0.1.36/vfs_fonts.js"></script>
+
+<script type="text/javascript" src="dependencias/DataTables-1.10.20/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="dependencias/Buttons-1.6.1/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="dependencias/Buttons-1.6.1/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="dependencias/Buttons-1.6.1/js/buttons.print.min.js"></script>
+<script type="text/javascript" src="dependencias/Responsive-2.2.3/js/dataTables.responsive.min.js"></script>
+<script src="dependencias/js/sweetalert2.all.min.js" crossorigin="anonymous"></script>
